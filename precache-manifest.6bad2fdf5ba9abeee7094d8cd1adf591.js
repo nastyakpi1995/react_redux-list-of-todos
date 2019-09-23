4e2c5a4b5a1a5348cecc52d07b79aa431b2b330b
Nastia Krat <nastyakpi1995@gmail.com>
2019-09-23 17:29:03 +0300

    Updates

---
+++ precache-manifest.6bad2fdf5ba9abeee7094d8cd1adf591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2030e9760e87da19e19c02b5a9d6ec2",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "f8cb6c41f32c8c118aec",
+    "url": "/react_redux-list-of-todos/static/css/main.0f1ec21f.chunk.css"
+  },
+  {
+    "revision": "06c7cfe730d4b17062ed",
+    "url": "/react_redux-list-of-todos/static/js/2.068de14d.chunk.js"
+  },
+  {
+    "revision": "f8cb6c41f32c8c118aec",
+    "url": "/react_redux-list-of-todos/static/js/main.92234058.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
